e9bf23ec7d0c83c91a04f85670c533dcbbbaf3f1
Rafael Bertolim Pavanello <rpavanello97@hotmail.com>
2021-04-23 22:04:41 -0300

    class 3

--- src/pages/episodes/[slug].tsx
+++ src/pages/episodes/[slug].tsx
@@ -2,9 +2,13 @@ import { parseISO } from 'date-fns';
 import format from 'date-fns/format';
 import { ptBR } from 'date-fns/locale';
 import { GetStaticPaths, GetStaticProps } from 'next';
+import Image from 'next/image';
+import Link from 'next/link';
 import { useRouter } from 'next/router'
 import { api } from '../../services/api';
 import { converDurationToTimeString } from '../../utils/convertDurationToTimeString';
+
+import styles from '../episodes/episode.module.scss';
 
 type Episode = {
     id: string;
@@ -26,7 +30,37 @@ export default function Episode({ episode }: EpisodeProps) {
     const router = useRouter();
 
     return (
-        <h1>{episode.title}</h1>
+        <div className={styles.episode}>
+            <div className={styles.thumbnailContainer}>
+                <Link href="/">
+                    <button type="button">
+                        <img src="/arrow-left.svg" alt="Voltar" />
+                    </button>
+                </Link>
+                <Image
+                    width={700}
+                    height={160}
+                    src={episode.thumbnail}
+                    objectFit="cover"
+                />
+                <button type="button">
+                    <img src="/play.svg" alt="Tocar episódio" />
+                </button>
+            </div>
+
+            <header>
+                <h1>{episode.title}</h1>
+                <span>{episode.members}</span>
+                <span>{episode.publishedAt}</span>
+                <span>{episode.durationAsString}</span>
+            </header>
+
+            <div
+                className={styles.description}
+                dangerouslySetInnerHTML={{ __html: episode.description }}
+            />
+
+        </div>
     )
 }
 
